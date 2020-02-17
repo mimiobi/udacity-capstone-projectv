a3d2d4773b76548971c84ac7cbc92e2b77c3c169
Michelle Onyeoziri <49154987+mimiobi@users.noreply.github.com>
2020-02-18 00:54:10 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -3,11 +3,11 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=udacity-capstone .
+docker build --tag=mimiobi/udacity-capstone .
 
 # Step 2: 
 docker image ls
 
 # Step 3: 
-docker run -p 8000:80 udacity-capstone
+docker run -p 8000:80 mimiobi/udacity-capstone
 
